df3ff9a991c57f65042305f21e40397f07246515
Seth <neaseth@deloitte.com>
2020-02-11 21:12:14 -0500

    init

---
+++ js/view.js
@@ -0,0 +1,26 @@
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// let geometry = new THREE.BoxGeometry(2, 2, 2);
+let geometry = new THREE.IcosahedronGeometry(2, 0);
+let material = new THREE.MeshNormalMaterial();
+let cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
